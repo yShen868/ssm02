9170d876e094da04186a7d3b04142b9f82522428
yShen868 <55044262+yShen868@users.noreply.github.com>
2019-12-13 10:18:14 +0800

    Update IAdminDao.java

--- src/main/java/com/yShen/study/dao/IAdminDao.java
+++ src/main/java/com/yShen/study/dao/IAdminDao.java
@@ -3,6 +3,7 @@ package com.yShen.study.dao;
 import com.yShen.study.model.Admin;
 
 public interface IAdminDao {
+    // 测试是否成功
     //    void add(Admin admin);
     Admin selectone(String username);
 }
